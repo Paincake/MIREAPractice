429267054f06c5f9354923359f8fcdc5d28f0521
Paincake <53565166+Paincake@users.noreply.github.com>
2022-10-15 16:49:53 +0300

    Update Main.java

--- ikb/src/main/java/ru/ryazhev/task/Main.java
+++ ikb/src/main/java/ru/ryazhev/task/Main.java
@@ -12,6 +12,13 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args)  {
-
+        CompanyStats stats = new CompanyStats("C:\\Users\\ryazh\\Desktop\\Stuff.csv");
+        System.out.println("Всего Иванов: " + stats.nameStat("Иван"));
+        System.out.println("Всего женщин: " + stats.sexStat(true));
+        System.out.println("Всего мужчин: " + stats.sexStat(false));
+        System.out.println("Родилось в сентябре: " + stats.birthMonthStat(9));
+        System.out.println("Зарплата <40000: " + stats.salaryStat(40000));
+        System.out.println("Зарплата между 40000 и 80000: " + stats.salaryStat(40000, 80000));
+        System.out.println("Зарплата >80000: " + stats.salaryStat(80000));
     }
 }
